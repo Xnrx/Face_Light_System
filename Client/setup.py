6506949ff7c04256eb06e6461cd04e27942e1120
zhongzhaojun <1194325548@qq.com>
2023-04-10 20:47:17 +0800

    qt5-添加arduino通信模块（未测试是否稳定）

--- Client/setup.py
+++ Client/setup.py
@@ -3,14 +3,20 @@ import sys
 import cv2
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QObject
-
+import User
 from CameraSelector import CameraSelector
 from InitRecognizerSys import InitRecognizerSys
 from UI_MainWindow3 import Ui__MainWindow
 
 
+class ArduinoThread(QtCore.QThread):
+    def __init__(self):
+        super(ArduinoThread, self).__init__()
+
+
 class CameraThread(QtCore.QThread):
     image_updated = QtCore.pyqtSignal(QtGui.QImage)
+    user_updated = QtCore.pyqtSignal(User.User)
 
     def __init__(self, camera_index, url):
         super(CameraThread, self).__init__()
@@ -36,6 +42,7 @@ class CameraThread(QtCore.QThread):
             show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
             showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0], QtGui.QImage.Format_RGB888)
             self.image_updated.emit(showImage)  # emit a signal to update the GUI with the new image
+            self.user_updated.emit(self.user)
         self.cap.release()
 
     def stop(self):
@@ -50,20 +57,20 @@ class MainWindow(Ui__MainWindow, QtWidgets.QWidget):
         self.timer_camera = QtCore.QTimer()
         self.slot_init()
 
+    # 两个按钮信号槽初始化
     def slot_init(self):
         self.button_open_camera.clicked.connect(self.button_open_camera_clicked)
         self.button_close.clicked.connect(self.close)
-        # self.camera_thread.image_updated.connect(self.update_image)
 
     def button_open_camera_clicked(self):
         if not self.camera_thread.is_running:
-            self.camera_thread.image_updated.connect(self.update_image)
+            self.camera_thread.image_updated.connect(self.update_image)  # 槽连接信号
             self.camera_thread.is_running = True
             self.camera_thread.start()
             self.button_open_camera.setText('关闭摄像头')
         else:
             self.camera_thread.stop()
-            self.camera_thread.image_updated.disconnect(self.update_image)
+            self.camera_thread.image_updated.disconnect(self.update_image)  # 槽断开连接信号
             self.label_show_camera.clear()
             self.label_faceInfo.clear()
             self.button_open_camera.setText('打开摄像头')
@@ -71,6 +78,7 @@ class MainWindow(Ui__MainWindow, QtWidgets.QWidget):
     def update_image(self, image):
         self.label_show_camera.setPixmap(QtGui.QPixmap.fromImage(image))
         self.label_faceInfo.setText(self.camera_thread.user.user_id)
+        self.camera_thread.faceSys.receive_and_send_signal(self.camera_thread.user)
 
 
 if __name__ == '__main__':
