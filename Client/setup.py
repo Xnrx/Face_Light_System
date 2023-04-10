1771540d8e950b0592984342ddc28364be497efa
zhongzhaojun <1194325548@qq.com>
2023-04-10 09:41:46 +0800

    添加了人脸识别信息文本

--- Client/setup.py
+++ Client/setup.py
@@ -4,7 +4,7 @@ import cv2
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from CameraSelector import CameraSelector
-from UI_MainWindow2 import Ui__MainWindow
+from UI_MainWindow3 import Ui__MainWindow
 
 
 class MainWindow(Ui__MainWindow, QtWidgets.QWidget):
@@ -38,7 +38,7 @@ class MainWindow(Ui__MainWindow, QtWidgets.QWidget):
     def show_camera(self):
         self.image = self.cap.get_frame()
 
-        show = cv2.resize(self.image, (768, 576))  # 把读到的帧的大小重新设置为 640x480
+        show = cv2.resize(self.image, (896, 672))  # 把读到的帧的大小重新设置为 640x480
         show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)  # 视频色彩转换回RGB，这样才是现实的颜色
         showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0], QtGui.QImage.Format_RGB888)  # 把读取到的视频数据变成QImage形式
         self.label_show_camera.setPixmap(QtGui.QPixmap.fromImage(showImage))  # 往显示视频的Label里 显示QImage
